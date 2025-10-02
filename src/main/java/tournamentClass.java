f2626e9c5aac57e178e4aa275b861ef5dacfdc8a
kendrickgray24-boop <kendrickgray24@augustana.edu>
2025-10-01 19:37:10 -0500

    Just a test

--- src/main/java/tournamentClass.java
+++ src/main/java/tournamentClass.java
@@ -4,8 +4,32 @@ public class tournamentClass {
 
     private ArrayList courtList = new ArrayList();
 
-    public String courtSorting(String teamName){
-        String court = "Temp Court name";
+    public  ArrayList getCourtList() {
+        return courtList;
+    }
+
+    public String groupCourtSorting(String[] teamName){
+
+        String court = courtAssignment();
+        // asignment to all members of team
+        // record name and make new list for court check to compare too
         return court;
     }
+
+    public String singleCourtSorting(String name){
+
+        String court = courtAssignment();
+
+        return court;
+    }
+
+    private String courtAssignment(){
+        // asign a court at random
+        return "No courts at the moment.";
+    }
+
+    private Boolean courtCheck(){
+       return true;
+    }
+    /// need method to asign courts and check that courts arnt already in use
 }
