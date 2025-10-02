21989d1f931377ad32709025de1eb3795268bda6
kendrickgray24-boop <kendrickgray24@augustana.edu>
2025-10-01 19:21:33 -0500

    Just a test

---
+++ src/main/java/tournamentClass.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class tournamentClass {
+
+    private ArrayList courtList = new ArrayList();
+
+    public String courtSorting(String teamName){
+        String court = "Temp Court name";
+        return court;
+    }
+}
